3c57d2e8c8b3ecf4bbf4d49d871c4b6de45cdda3
andrei33333 <stefanandreiismana@gmail.com>
2022-07-06 12:19:48 +0300

    doo

--- Assets/scripts/patrat2.cs
+++ Assets/scripts/patrat2.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 
 public class patrat2 : MonoBehaviour
 {
+    public int row, column;
+
     // Start is called before the first frame update
     private void OnMouseDown()
     {
-        if (patrat1.table[0, 1] == -1)
+        if (patrat1.table[row, column] == -1)
         {
             transform.GetChild(0).GetComponent<TMP_Text>().text = patrat1.counter % 2 == 0 ? "X" : "0";
-            patrat1.table[0, 1] = patrat1.counter % 2;//daca e 0 e x altfel daca e 1 e zero
+            patrat1.table[row, column] = patrat1.counter % 2;//daca e 0 e x altfel daca e 1 e zero
             patrat1.counter++;
             patrat1.lst[patrat1.lp++] = 0;
             patrat1.cst[patrat1.cp++] = 1;
--- Assets/scripts/resetscript.cs
+++ Assets/scripts/resetscript.cs
@@ -12,14 +12,6 @@ public class resetscript : MonoBehaviour
                 patrat1.table[i, j] = -1;
         GameObject.Find("patrat#1").GetComponent<Transform>().GetChild(0).GetComponent<TMP_Text>().text = " ";
         GameObject.Find("patrat#2").GetComponent<Transform>().GetChild(0).GetComponent<TMP_Text>().text = " ";
-        GameObject.Find("patrat#3").GetComponent<Transform>().GetChild(0).GetComponent<TMP_Text>().text = " ";
-        GameObject.Find("patrat#4").GetComponent<Transform>().GetChild(0).GetComponent<TMP_Text>().text = " ";
-        GameObject.Find("patrat#5").GetComponent<Transform>().GetChild(0).GetComponent<TMP_Text>().text = " ";
-        GameObject.Find("patrat#6").GetComponent<Transform>().GetChild(0).GetComponent<TMP_Text>().text = " ";
-        GameObject.Find("patrat#7").GetComponent<Transform>().GetChild(0).GetComponent<TMP_Text>().text = " ";
-        GameObject.Find("patrat#8").GetComponent<Transform>().GetChild(0).GetComponent<TMP_Text>().text = " ";
-        GameObject.Find("patrat#9").GetComponent<Transform>().GetChild(0).GetComponent<TMP_Text>().text = " ";
-        GameObject.Find("patrat#1").GetComponent<Transform>().GetChild(1).GetComponent<TMP_Text>().text = " ";
         patrat1.iswon = 0;
         patrat1.ngames++;
     }
